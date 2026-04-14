f27801628e0a45fd170cb4a501d65b828027bdf7
DiegoRuiz0109zzz <N00281937@upn.pe>
2026-04-14 18:06:31 -0500

    cambio produccion

--- src/pages/util/baseUrl.js
+++ src/pages/util/baseUrl.js
@@ -1,9 +1,9 @@
-const localhostEndpoint = "http://localhost:8080/api/";
+//const localhostEndpoint = "http://localhost:8080/api/";
 //const ipEndpoint = "http://172.17.221.214:8080/api/";
-// const productionEndpoint = "";
+const productionEndpoint = "http://161.132.47.150/api/";
 
 //se cambia la base URL
-const defaultEndpoint = localhostEndpoint;
+const defaultEndpoint = productionEndpoint;
 
 const restrictions = {
     MIN_DATE_TO_MAKE_ORDER: new Date()
