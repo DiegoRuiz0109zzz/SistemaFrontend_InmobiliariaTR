26a4d4f87f2fe03b2035597ae7fff1a25356ba6c
DiegoRuiz0109zzz <N00281937@upn.pe>
2026-04-17 15:32:47 -0500

    cambios en la url

--- src/pages/util/baseUrl.js
+++ src/pages/util/baseUrl.js
@@ -1,6 +1,6 @@
 //const localhostEndpoint = "http://localhost:8080/api/";
 //const ipEndpoint = "http://172.17.221.214:8080/api/";
-const productionEndpoint = "http://inmobiliariaterranort.net.pe/api/";
+const productionEndpoint = "https://inmobiliariaterranort.net.pe/api/";
 //const productionEndpoint = "http://161.132.47.150/api/";
 
 
